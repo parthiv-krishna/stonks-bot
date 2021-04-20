02c67919e5cbacd05b6da6f259928270d8356f8c
Parthiv Krishna <parthiv-krishna@users.noreply.github.com>
2021-04-19 18:44:18 -0700

    Stock info in chat from finnhub

--- README.md
+++ README.md
@@ -3,8 +3,5 @@ Discord bot with S&amp;P 500 info
 
 ## Usage
 * Create a bot following [these instructions](https://discordpy.readthedocs.io/en/stable/discord.html#discord-intro).
-* Update the information in `sample.env`. The `PERMISSIONS=67111952` option is set by default to have the following permissions:
-    * Manage Channels
-    * Change Nickname
-    * View Channels
-    * Send Messages
+* Obtain a [Finnhub](https://finnhub.io) API key.
+* Update the information in `sample.env`.
--- stonks-bot.py
+++ stonks-bot.py
@@ -1,12 +1,17 @@
 import discord
 import os
 from dotenv import load_dotenv
+import requests
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+FINNHUB_KEY = os.getenv('FINNHUB_KEY')
+STONKS_EMOJI = os.getenv('STONKS_EMOJI')
+UNSTONKS_EMOJI = os.getenv('UNSTONKS_EMOJI')
 
 client = discord.Client()
-GUILD = os.getenv('DISCORD_GUILD')
+
 
 @client.event
 async def on_ready():
@@ -17,7 +22,28 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    if message.content.startswith('stonks'):
+        tokens = message.content.strip().split(' ')
+        for token in tokens[1:]:
+            ticker = token.upper()
+            await ticker_message(ticker, message)
+
+def get_quote(symbol):
+    url = 'https://finnhub.io/api/v1/quote?symbol=' + symbol + '&token=' + FINNHUB_KEY
+    response = requests.get(url)
+    quote = response.json()
+    change = round(quote['c'] - quote['pc'], 2)
+    quote['change'] = '+$' + str(change) if change > 0 else '-$' + str(abs(change))
+    percent = round((((quote['c'] / quote['pc']) - 1)*100), 2)
+    quote['percent'] = '+' + str(percent) if percent > 0 else str(percent)
+    quote['symbol'] = symbol
+    quote['emoji'] = STONKS_EMOJI if change > 0 else UNSTONKS_EMOJI
+    return quote
+    
+async def ticker_message(ticker, message):
+    quote = get_quote(ticker)
+    msg  = "Quote for **{symbol}**: ${c:.2f} ({change} {emoji} {percent}%) O: {o:.2f} H: ${h:.2f} L: ${l:.2f} PC: ${pc:.2f}".format(**quote)
+    print(msg)
+    await message.channel.send(msg)
 
 client.run(TOKEN)
